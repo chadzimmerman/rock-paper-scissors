6e97119c75be54cd9d94e67ed9537d8ee0df1ae6
Chad Zimmerman <137725897+chadzimmerman@users.noreply.github.com>
2023-08-06 10:00:29 -0600

    getRandom and getComputerChoice intail build
    1. getRandom successfully tested and working for returning random number
    between 1-100.
    2. getComputerChoice is returning "undefined" for "result". Currently
    "let result;" is copied from a MDN article, but it seems it needs
    definition. Asked discord TOP community for help defining it.
    3. Earlier code iteration returned "Scissors", the last else clause,
    every time. Not seeing that now as its only returning "undefined".
    Probably will come back up once definition is solved.

---
+++ javascript.js
@@ -0,0 +1,36 @@
+// getComputerChoice function will return either rock, paper or scissors
+// two parameters: playerSelection (should be case insensitive) and computerSelection
+// winner of the round declares, "You Lose! Paper beats Rock"
+// function should RETURN and not CONSOLE.LOG the result. 
+
+// 1. define variables of rock, paper and scissors.
+// 2. computerSelection chooses random variable of either rock, paper or else scissors. 
+// 3. playerSelection receives input from player of a variable of rock, paper or scissors. 
+// 4. function will determine which variable wins over the other variable. 
+// 5. function will return the slogan "You Lose! Paper beats Rock", "You Lose! Rock beats Scissors" 
+//or "You Lose! Scissors beats Paper"
+
+function getRandom() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function getComputerChoice() {
+    let result;
+    if (getRandom <= 33) {
+    result = "Rock";
+    } else if (getRandom > 33 && getRandom <= 66) {
+    result = "Paper";
+    } else if (getRandom > 66 && getRandom <= 100) {
+    result = "Scissors";
+    }
+  return alert(result);
+}
+  console.log(getComputerChoice());
+
+  //function playRound(playerSelection, computerSelection) {
+    // your code here!
+  //}
+
+//function playerSelection() {}
+
+//function computerSelection() {}
